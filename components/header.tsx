3708806049812644fad491a3b4d9bec2f8d8349a
Fernando Panduro <fernandogp2003pro@gmail.com>
2023-10-30 20:04:49 -0700

    🌈 style: Header margin

--- components/header.tsx
+++ components/header.tsx
@@ -6,23 +6,25 @@ type Props = {};
 
 const Header = (props: Props) => {
   return (
-    <header className="w-full flex items-center justify-between px-6 py-3 fixed top-0 left-0 z-50 backdrop-blur-md border-b border-zinc-200">
-      <Link href={"/"}>
-        <Logo />
-      </Link>
-      <nav>
-        <ul className="flex items-center gap-4">
-          <li>
-            <Link href={"/"}> Home</Link>
-          </li>
-          <li>
-            <Link href={"/youtube"}>Youtube AI</Link>
-          </li>
-          <li>
-            <Button>Login</Button>
-          </li>
-        </ul>
-      </nav>
+    <header className="w-full px-6 py-3 lg:px-8 fixed top-0 left-0 z-50 backdrop-blur-md border-b border-zinc-200">
+      <div className="mx-auto max-w-7xl flex items-center justify-between">
+        <Link href={"/"}>
+          <Logo />
+        </Link>
+        <nav>
+          <ul className="flex items-center gap-4">
+            <li>
+              <Link href={"/"}> Home</Link>
+            </li>
+            <li>
+              <Link href={"/youtube"}>Youtube AI</Link>
+            </li>
+            <li>
+              <Button>Login</Button>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 };
