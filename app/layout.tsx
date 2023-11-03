b5ceba7ccaf6dca01a23ba18547b52bc51274946
Fernando Panduro <fernandogp2003pro@gmail.com>
2023-11-03 11:47:01 -0700

    ✨ feat: Change favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,6 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        
         <body className={inter.className}>
           <ThemeProvider
             attribute="class"
