655a7a8d3ffd789834a8fb293423da3662975047
Fernando Panduro <fernandogp2003pro@gmail.com>
2023-11-03 12:01:06 -0700

    ✨ feat: Add analytics vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,10 @@
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/toaster";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Analytics } from "@vercel/analytics/react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -27,6 +29,7 @@ export default function RootLayout({
             enableSystem
             disableTransitionOnChange>
             {children}
+            <Analytics />
             <Toaster />
           </ThemeProvider>
         </body>
